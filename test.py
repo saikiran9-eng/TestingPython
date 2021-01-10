805256f8c70c473feb8fd01c3663d8cddc1b958f
Administrator <saikirankotawar@gmail.com>
2021-01-10 19:29:30 +0530

    One Line is added Onemore Line is added New code was added

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-print("Hello to GitHub")
-print("Phyton is a Powerful Programming Langauage and Scrripting")
-print("Will get a Job in Infosys")
+
+names = ("Navin", "saikiran", "Tendulkar", "saikiran")
+company = ("Microsoft", "Infosys", "cognizant", "Wipro")
+location = ("Chennai", "Banglore", "Delhi", "Hyderabad")
+
+zipped = zip(names, company, location)
+
+for (a,b,c) in zipped:
+    print(a,b,c)
